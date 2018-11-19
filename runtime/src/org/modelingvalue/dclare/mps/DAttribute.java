6fe11c4fe8d8599637b02cd873f24407a1d52b0f
Wim Bast <wim.bast@gmail.com>
2018-11-19 21:13:55 +0100

    ClassRulesSets

--- runtime/src/org/modelingvalue/dclare/mps/DAttribute.java
+++ runtime/src/org/modelingvalue/dclare/mps/DAttribute.java
@@ -1,47 +1,126 @@
 package org.modelingvalue.dclare.mps;
 
+import java.util.function.BiFunction;
+
 import org.modelingvalue.collections.util.QuadConsumer;
-import org.modelingvalue.collections.util.Triple;
+import org.modelingvalue.collections.util.Quadruple;
 import org.modelingvalue.transactions.AbstractLeaf;
 import org.modelingvalue.transactions.ConstantSetable;
 import org.modelingvalue.transactions.Getable;
 
 @SuppressWarnings("rawtypes")
-public class DAttribute<O, T> extends DObserved<O, T> {
+public interface DAttribute<O, T> {
 
     @SuppressWarnings("unchecked")
-    private static final Getable<Triple<Object, String, Boolean>, DAttribute> ATTRIBUTE = ConstantSetable.of("ATTRIBUTE", id -> new DAttribute(id.a(), id.b(), id.c(), null, null));
+    static final Getable<Quadruple<Object, String, Boolean, Integer>, DAttribute> ATTRIBUTE = ConstantSetable.of("ATTRIBUTE", id -> {
+        return id.d() >= 0 ? new DIdentifyingAttribute(id.a(), id.b(), id.c(), id.d()) : new DObservedAttribut(id.a(), id.b(), id.c(), null);
+    });
 
     @SuppressWarnings("unchecked")
-    public static <C, V> DAttribute<C, V> of(Object id, String name, boolean composite) {
-        return ATTRIBUTE.get(Triple.of(id, name, composite));
+    public static <C, V> DAttribute<C, V> of(Object id, String name, boolean composite, int identifyingNr) {
+        return ATTRIBUTE.get(Quadruple.of(id, name, composite, identifyingNr));
     }
 
-    private String  name;
-    private boolean composite;
-
-    public DAttribute(Object id, String name, boolean composite, T def, QuadConsumer<AbstractLeaf, O, T, T> changed) {
-        super(id, def, false, false, (o, b, a) -> {
-        }, changed);
-        this.name = name;
-        this.composite = composite;
+    @SuppressWarnings("unchecked")
+    public static <C, V> DAttribute<C, V> of(Object id, String name, boolean composite, QuadConsumer<AbstractLeaf, C, V, V> changed) {
+        return new DObservedAttribut(id, name, composite, changed);
     }
 
-    @Override
-    public T get(O object) {
-        T result = object == null ? null : super.get(object);
-        if (object != null && result == null) {
-            DObject.EMPTY_ATTRIBUTE.set(true);
+    T get(O object);
+
+    T set(O object, T value);
+
+    <E> T set(O object, BiFunction<T, E, T> function, E element);
+
+    boolean isComposite();
+
+    final static class DObservedAttribut<C, V> extends DObserved<C, V> implements DAttribute<C, V> {
+
+        private String  name;
+        private boolean composite;
+
+        public DObservedAttribut(Object id, String name, boolean composite, QuadConsumer<AbstractLeaf, C, V, V> changed) {
+            super(id, null, false, false, (o, b, a) -> {
+            }, changed);
+            this.name = name;
+            this.composite = composite;
         }
-        return result;
+
+        @Override
+        public V get(C object) {
+            V result = object == null ? null : super.get(object);
+            if (object != null && result == null) {
+                DObject.EMPTY_ATTRIBUTE.set(true);
+            }
+            return result;
+        }
+
+        @Override
+        public String toString() {
+            return name;
+        }
+
+        @Override
+        public boolean isComposite() {
+            return composite;
+        }
     }
 
-    @Override
-    public String toString() {
-        return name;
-    }
+    final static class DIdentifyingAttribute<C, V> implements DAttribute<C, V> {
+        private Object  id;
+        private String  name;
+        private boolean composite;
+        private int     index;
 
-    public boolean isComposite() {
-        return composite;
+        public DIdentifyingAttribute(Object id, String name, boolean composite, int index) {
+            this.id = id;
+            this.name = name;
+            this.composite = composite;
+            this.index = index;
+        }
+
+        @Override
+        public int hashCode() {
+            return id.hashCode();
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) {
+                return true;
+            } else if (!(obj instanceof DIdentifyingAttribute)) {
+                return false;
+            } else {
+                DIdentifyingAttribute other = (DIdentifyingAttribute) obj;
+                return id.equals(other.id);
+            }
+        }
+
+        @SuppressWarnings("unchecked")
+        @Override
+        public V get(C object) {
+            return (V) ((SClassObject) object).getIdentity()[index];
+        }
+
+        @Override
+        public String toString() {
+            return name;
+        }
+
+        @Override
+        public boolean isComposite() {
+            return composite;
+        }
+
+        @Override
+        public V set(C object, V value) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public <E> V set(C object, BiFunction<V, E, V> function, E element) {
+            throw new UnsupportedOperationException();
+        }
+
     }
 }
