762137016aee77afa4fc20310c320acc1c2c02c0
Wim Bast <wim.bast@gmail.com>
2018-12-14 09:22:40 +0100

    Optionality of attributes

--- runtime/src/org/modelingvalue/dclare/mps/DAttribute.java
+++ runtime/src/org/modelingvalue/dclare/mps/DAttribute.java
@@ -18,15 +18,17 @@ public interface DAttribute<O, T> {
 
         private static final long serialVersionUID = -403866123495976516L;
 
-        public static <X, Y> Key<X, Y> of(Object a, String b, Boolean c, Integer d, Function<X, Y> function) {
-            return new Key<X, Y>(a, b, c, d, function);
+        public static <X, Y> Key<X, Y> of(Object a, String b, boolean optional, boolean c, int d, Function<X, Y> function) {
+            return new Key<X, Y>(a, b, optional, c, d, function);
         }
 
         private final Function<C, V> function;
+        private final boolean        optional;
 
-        private Key(Object a, String b, Boolean c, Integer d, Function<C, V> function) {
+        private Key(Object a, String b, boolean optional, boolean c, int d, Function<C, V> function) {
             super(a, b, c, d);
             this.function = function;
+            this.optional = optional;
         }
 
         private Object id() {
@@ -49,22 +51,12 @@ public interface DAttribute<O, T> {
     @SuppressWarnings("unchecked")
     static final Getable<Key, DAttribute> ATTRIBUTE = Constant.of("ATTRIBUTE", key -> {
         return key.identifyingNr() >= 0 ? new DIdentifyingAttribute(key.id(), key.name(), key.composite(), key.identifyingNr()) : key.function != null ? //
-        new DConstant(key.id(), key.name(), key.composite(), key.function) : new DObservedAttribut(key.id(), key.name(), key.composite(), null, null);
+        new DConstant(key.id(), key.name(), key.composite(), key.function) : new DObservedAttribut(key.id(), key.name(), key.optional, key.composite(), null, null);
     });
 
     @SuppressWarnings("unchecked")
-    public static <C, V> DAttribute<C, V> of(Object id, String name, boolean composite, int identifyingNr, Function<C, V> deriver) {
-        return ATTRIBUTE.get(Key.of(id, name, composite, identifyingNr, deriver));
-    }
-
-    @SuppressWarnings("unchecked")
-    public static <C, V> DAttribute<C, V> of(Object id, String name, boolean composite, V def, QuadConsumer<AbstractLeaf, C, V, V> changed) {
-        return new DObservedAttribut(id, name, composite, def, changed);
-    }
-
-    @SuppressWarnings("unchecked")
-    public static <C, V> DAttribute<C, V> of(Object id, String name, boolean composite, Function<C, V> deriver) {
-        return new DConstant(id, name, composite, deriver);
+    public static <C, V> DAttribute<C, V> of(Object id, String name, boolean optional, boolean composite, int identifyingNr, Function<C, V> deriver) {
+        return ATTRIBUTE.get(Key.of(id, name, optional, composite, identifyingNr, deriver));
     }
 
     T get(O object);
@@ -84,8 +76,8 @@ public interface DAttribute<O, T> {
         private String  name;
         private boolean composite;
 
-        public DObservedAttribut(Object id, String name, boolean composite, V def, QuadConsumer<AbstractLeaf, C, V, V> changed) {
-            super(id, def, true, false, (o, b, a) -> {
+        public DObservedAttribut(Object id, String name, boolean optional, boolean composite, V def, QuadConsumer<AbstractLeaf, C, V, V> changed) {
+            super(id, def, !optional, false, (o, b, a) -> {
             }, changed);
             this.name = name;
             this.composite = composite;
