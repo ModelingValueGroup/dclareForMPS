4addfce22dc277cf5258d6a46ebdf1ef4679e36c
Wim Bast <wim.bast@gmail.com>
2019-01-28 16:38:08 +0100

    DProblems

---
+++ runtime/src/org/modelingvalue/dclare/mps/DProblem.java
@@ -0,0 +1,50 @@
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// (C) Copyright 2018 Modeling Value Group B.V. (http://modelingvalue.org)                                             ~
+//                                                                                                                     ~
+// Licensed under the GNU Lesser General Public License v3.0 (the "License"). You may not use this file except in      ~
+// compliance with the License. You may obtain a copy of the License at: https://choosealicense.com/licenses/lgpl-3.0  ~
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on ~
+// an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the  ~
+// specific language governing permissions and limitations under the License.                                          ~
+//                                                                                                                     ~
+// Contributors:                                                                                                       ~
+//     Wim Bast, Carel Bast, Tom Brus, Arjan Kok, Ronald Krijgsheld                                                    ~
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+package org.modelingvalue.dclare.mps;
+
+import java.util.function.Supplier;
+
+import org.jetbrains.mps.openapi.model.SNode;
+import org.modelingvalue.collections.util.Pair;
+
+@SuppressWarnings("rawtypes")
+public class DProblem extends Pair<DObject, Object> {
+    private static final long     serialVersionUID = -303368640776089669L;
+
+    private final Object          content;
+    private final Supplier<SNode> source;
+
+    public DProblem(DObject context, Object id, Supplier<SNode> source, Object content) {
+        super(context, id);
+        this.source = source;
+        this.content = content;
+    }
+
+    public DObject context() {
+        return a();
+    }
+
+    public Object id() {
+        return b();
+    }
+
+    public SNode source() {
+        return source.get();
+    }
+
+    public Object content() {
+        return content;
+    }
+
+}
