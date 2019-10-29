4b25712dce4bfde5e9f6fb34bb2b713c18c8b57d
Tom Brus <tombrus@gmail.com>
2019-10-29 23:09:40 +0100

    implements build

--- build/07-publish.sh
+++ build/07-publish.sh
@@ -3,7 +3,16 @@ set -ue
 . 00-vars.sh
 ls -l build/artifacts/DclareForMPS/DclareForMPS.zip
 ################################################################
-if [[ $release != "" && $release != SNAPHOT && $gitHubToken != "" && $gitHubToken != NO_TOKEN ]]; then
-  echo "...publish to github as release '$release'"
-  echo "...maybe later"
+if [[ $GITHUB_REF == master* ]]; then
+  TAG="release $DclareForMPS_version"
+  if [[ $GITHUB_REF != master ]]; then
+    TAG="TAG for ${GITHUB_REF/master-}"
+  fi
+  echo "...publish to github as $TAG"
+  if git rev-parse "$TAG" >/dev/null 2>&1; then
+    echo "[ERROR] the tag $TAG already exists so I guess this version has already been released. Update buildParameters.sh!"
+    exit 99
+  fi
+
+  echo "...to be implemented"
 fi
