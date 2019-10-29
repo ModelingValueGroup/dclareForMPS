9a962475116a143609d3886cd4e6fa6acd8667b1
Tom Brus <tombrus@gmail.com>
2019-10-29 14:38:45 +0100

    implements build

--- build/07-publish.sh
+++ build/07-publish.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -ue
 . 00-vars.sh
+ls -l build/artifacts/DclareForMPS/DclareForMPS.zip
 ################################################################
 if [[ $release != "" && $release != SNAPHOT && $gitHubToken != "" && $gitHubToken != NO_TOKEN ]]; then
   echo "...publish to github as release '$release'"
