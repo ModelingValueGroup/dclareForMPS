56112242409b7f6e4b67c28fd151f4786ff7fab0
Tom Brus <tombrus@gmail.com>
2019-10-29 14:33:28 +0100

    implements build

--- build/05-test.sh
+++ build/05-test.sh
@@ -3,24 +3,5 @@ set -ue
 . 00-vars.sh
 ################################################################
 if [ "$runTests" == true ]; then
-  echo "...testing"
-  generateAntTestFile "dclareForMps" > test.xml
-
-  sed 's/^/@@@ /' test.xml
-
-  if ! ant -Dpath.variable.maven_repository=$mavenReposDir -f test.xml; then
-    kill %1
-    echo "======================================================================"
-    echo " FAILURES DETECTED"
-    echo "======================================================================"
-    for f in $(\
-          egrep '(errors|failures)="' TEST-* \
-                | egrep -v 'errors="0" failures="0"' \
-                | sed 's/:.*//'
-          ); do
-          [[ -f "$f" ]] && cat $f
-    done
-    echo "======================================================================"
-    exit 99
-  fi
+  echo "... no tests yet"
 fi
