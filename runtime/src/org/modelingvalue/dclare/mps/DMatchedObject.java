999ef78b0c8287e89e5f0218d2991ccf883e12b5
WimBast <wim.bast@gmail.com>
2020-12-17 22:28:45 +0100

    cycle fix

--- runtime/src/org/modelingvalue/dclare/mps/DMatchedObject.java
+++ runtime/src/org/modelingvalue/dclare/mps/DMatchedObject.java
@@ -61,7 +61,7 @@ public abstract class DMatchedObject<T extends DMatchedObject, R, S> extends DId
         Setable<Mutable, R> uisetable = (Setable<Mutable, R>) UNIDENTIFIED_CHILDREN.get(Pair.of(setable, tx.leaf()));
         if (tx instanceof ObserverTransaction && !pres.equals(posts)) {
             R result = posts;
-            List<C> postList = posts.filter(p -> !p.isRead() && p.isDerived() && !pres.contains(p)).toList();
+            List<C> postList = posts.filter(p -> !p.isRead() && !pres.contains(p)).toList();
             R unidentified = (R) uisetable.getDefault().addAllUnique(postList.filter(DMatchedObject::unidentified));
             uisetable.set(parent, unidentified);
             if (!unidentified.isEmpty()) {
@@ -100,7 +100,7 @@ public abstract class DMatchedObject<T extends DMatchedObject, R, S> extends DId
         LeafTransaction tx = LeafTransaction.getCurrent();
         Setable<Mutable, C> uisetable = (Setable<Mutable, C>) UNIDENTIFIED_CHILDREN.get(Pair.of(setable, tx.leaf()));
         if (tx instanceof ObserverTransaction && !Objects.equals(pre, post)) {
-            if (pre != null && post != null && pre.isRead() && !post.isRead() && post.isDerived()) {
+            if (pre != null && post != null && pre.isRead() && !post.isRead()) {
                 C unidentified = post.unidentified() ? post : null;
                 uisetable.set(parent, unidentified);
                 if (unidentified != null) {
@@ -173,10 +173,6 @@ public abstract class DMatchedObject<T extends DMatchedObject, R, S> extends DId
 
     protected boolean isRead() {
         return constructions().anyMatch(DReadConstruction.class::isInstance);
-    }
-
-    protected boolean isDerived() {
-        return constructions().anyMatch(DDeriveConstruction.class::isInstance);
     }
 
     protected abstract Object matchType();
