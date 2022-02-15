2c180d71ad876f47ab371fd21439c0f3365439be
Ronald Krijgsheld <rkrijgsheld@gmail.com>
2022-02-15 15:47:17 +0100

    dclare trace

---
+++ runtime/src/main/java/org/modelingvalue/dclare/mps/DclareTraceBroadcaster.java
@@ -0,0 +1,35 @@
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// (C) Copyright 2018-2022 Modeling Value Group B.V. (http://modelingvalue.org)                                        ~
+//                                                                                                                     ~
+// Licensed under the GNU Lesser General Public License v3.0 (the 'License'). You may not use this file except in      ~
+// compliance with the License. You may obtain a copy of the License at: https://choosealicense.com/licenses/lgpl-3.0  ~
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on ~
+// an 'AS IS' BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the  ~
+// specific language governing permissions and limitations under the License.                                          ~
+//                                                                                                                     ~
+// Maintainers:                                                                                                        ~
+//     Wim Bast, Tom Brus, Ronald Krijgsheld                                                                           ~
+// Contributors:                                                                                                       ~
+//     Arjan Kok, Carel Bast                                                                                           ~
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+package org.modelingvalue.dclare.mps;
+
+import org.modelingvalue.collections.Set;
+
+public class DclareTraceBroadcaster {
+	
+	private static DclareTracer tracer;
+	public static void setTracer(DclareTracer tracer) {
+		DclareTraceBroadcaster.tracer = tracer;
+	}
+	
+	public static void onModelActive(DModel m) {
+		tracer.onModelActive(m);
+	}
+	
+	public static void onRuleSetActive(Set<IRuleSet> r) {
+		r.forEach(i-> tracer.onRuleSetActive(i));		
+	}
+
+}
