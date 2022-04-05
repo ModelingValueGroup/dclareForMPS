87cdaac0c05751d60d65860156cd801a91e92420
Ronald Krijgsheld <rkrijgsheld@gmail.com>
2022-04-05 16:49:14 +0200

    remove tracing

--- runtime/src/main/java/org/modelingvalue/dclare/mps/DServerMetaData.java
+++ runtime/src/main/java/org/modelingvalue/dclare/mps/DServerMetaData.java
@@ -24,27 +24,26 @@ import org.modelingvalue.dclare.Setable;
 
 public class DServerMetaData extends DObject {
 
-	
+
 	protected static final Constant<Boolean, DServerMetaDataType>                       SERVER_METADATA_TYPE      = Constant.of("SERVER_METADATA_TYPE", p -> new DServerMetaDataType(p));
 
 	protected static final DObserved<DServerMetaData, Set<DModel>>                      SHARED_MODELS             = DObserved.of("SHARED_MODELS", Set.of(), (m,pre,post)->false);
-	
+
 	protected static final Observer<DServerMetaData>                                    MODELS_RULE               = DObject.observer("$SHARED_MODELS", o-> {
 																														 Set<DModel> sharedModels = DRepository.MODULES.get(dClareMPS().getRepository()).filter(m -> !m.isExternal()).flatMap(m-> DModule.MODELS.get(m)).filter(m->m.isShared()).toSet();
-																														 System.err.println("shared models: " + sharedModels);
 																														 SHARED_MODELS.set(o, sharedModels);
 																													 });
-	
+
 	@SuppressWarnings("rawtypes")
 	protected static final Set<Observer>                                              OBSERVERS                 = DObject.OBSERVERS.add(MODELS_RULE);
 
     @SuppressWarnings("rawtypes")
     protected static final Set<Setable>                                               SETABLES                  = DObject.SETABLES.addAll(Set.of(SHARED_MODELS));
 
-   
+
     public DServerMetaData() {
 	}
-    
+
 	@Override
 	protected void read(DClareMPS dClareMPS) {
 		// TODO Auto-generated method stub
