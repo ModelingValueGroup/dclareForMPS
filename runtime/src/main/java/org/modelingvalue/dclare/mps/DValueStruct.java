d9cfa0a200cfe888e3d61dbf81e9ec7e510ec47b
automation <automation@modelingvalue.org>
2026-01-09 13:16:47 +0000

    [no-ci] updated by mvgplugin

--- runtime/src/main/java/org/modelingvalue/dclare/mps/DValueStruct.java
+++ runtime/src/main/java/org/modelingvalue/dclare/mps/DValueStruct.java
@@ -1,5 +1,5 @@
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-//  (C) Copyright 2018-2025 Modeling Value Group B.V. (http://modelingvalue.org)                                         ~
+//  (C) Copyright 2018-2026 Modeling Value Group B.V. (http://modelingvalue.org)                                         ~
 //                                                                                                                       ~
 //  Licensed under the GNU Lesser General Public License v3.0 (the 'License'). You may not use this file except in       ~
 //  compliance with the License. You may obtain a copy of the License at: https://choosealicense.com/licenses/lgpl-3.0   ~
