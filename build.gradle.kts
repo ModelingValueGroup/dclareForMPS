0187fb6f4452d2c78a45abc951fc74ab644a41d2
Tom Brus <tombrus@gmail.com>
2026-03-02 13:55:34 +0100

    bump plugin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ defaultTasks(
         "mvguploader",
             )
 plugins {
-    id("org.modelingvalue.gradle.mvgplugin") version "2.3.18"
+    id("org.modelingvalue.gradle.mvgplugin") version "2.3.19"
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
